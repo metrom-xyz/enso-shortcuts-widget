f6315f8aae980d3d96678b921b3a36f1742bfa89
pixelis <zhadkov@gmail.com>
2025-01-10 21:14:35 +0100

    allow theme config

--- widget/index.tsx
+++ widget/index.tsx
@@ -1,19 +1,31 @@
 import SwapWidget from "@/components/SwapWidget";
 import { Address, WidgetProps } from "@/types";
-import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  SystemConfig,
+} from "@chakra-ui/react";
 import { useStore } from "@/store";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
-export const system = createSystem(defaultConfig, {
-  disableLayers: true,
-});
+export { type SystemConfig };
 
 export default ({
   apiKey,
   obligatedTokenOut,
   obligatedChainId,
-}: WidgetProps) => {
+  themeConfig,
+}: WidgetProps & { themeConfig?: SystemConfig; obligatedChainId?: number }) => {
   const { setObligatedChainId } = useStore();
+
+  const system = useMemo(
+    () =>
+      createSystem(defaultConfig, themeConfig, {
+        disableLayers: true,
+      }),
+    [themeConfig],
+  );
 
   useEffect(() => {
     setObligatedChainId(obligatedChainId);
--- widget/src/types.ts
+++ widget/src/types.ts
@@ -3,5 +3,4 @@ export type Address = `${"0x"}${string}`;
 export type WidgetProps = {
   apiKey: string;
   obligatedTokenOut?: Address;
-  obligatedChainId?: number;
 };

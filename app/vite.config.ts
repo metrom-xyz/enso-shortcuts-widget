a680015dcaec1ee9325138ce1878260fe22fb84b
pixelis <zhadkov@gmail.com>
2025-02-05 19:05:46 +0100

    fix base path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// @ts-expect-error  env is accessible
-const basePath = process.env.VITE_BASE_PATH;
-
-console.log(basePath);
 // https://vite.dev/config/
-export default defineConfig({
-  base: basePath,
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  // @ts-expect-error  env is accessible
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
+  };
 });

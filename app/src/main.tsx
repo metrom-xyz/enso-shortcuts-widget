a1c2cd2fa766d715a720d9bf2961b481b33f79d7
pixelis <zhadkov@gmail.com>
2025-02-12 14:26:05 +0100

    support vercel route

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
